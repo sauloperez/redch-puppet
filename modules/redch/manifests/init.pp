77e13035c22a2f9fb45bfd58743e967308924e9b
Pau Pérez <saulopefa@gmail.com>
2014-03-28 08:30:57 +0100

    Install redch upstart script from redch module

--- modules/redch/manifests/init.pp
+++ modules/redch/manifests/init.pp
@@ -8,6 +8,13 @@ class redch {
     group => ubuntu,
     mode => 775
   }
+  file { "/etc/init/redch.conf":
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 644,
+    source => "puppet:///modules/redch/redch.conf"
+  }
   package {
     "bundler":
       provider => gem
