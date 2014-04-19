dcf7ca7db5800b93d99655762974fb931ecfa69b
Pau Pérez <saulopefa@gmail.com>
2014-04-19 20:28:11 +0200

    Symlink redch.properties from shared/config

--- modules/sos/manifests/init.pp
+++ modules/sos/manifests/init.pp
@@ -2,4 +2,16 @@ class sos {
   exec { 'adduser ubuntu admin' }
   exec { 'chgrp admin /var/www' }
   exec { 'chmod g+x /var/www' }
+
+  file { "/var/www/sos/shared/config":
+    ensure => directory,
+    owner  => ubuntu,
+    group  => ubuntu,
+    mode   => 750
+  }
+
+  file { "/var/www/sos/shared/config/redch.properties":
+    ensure => link,
+    target => "/home/ubuntu/redch.properties"
+  }
 }
