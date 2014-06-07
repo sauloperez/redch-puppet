4208a83d0ce8d0a363217b301785d2fdcf70b95c
Pau Pérez <saulopefa@gmail.com>
2014-06-07 19:04:47 +0200

    Set proper permissions for the user used by capistrano

--- modules/redch/manifests/init.pp
+++ modules/redch/manifests/init.pp
@@ -1,13 +1,22 @@
 class redch {
-  file { "/var/redch/shared/env_ruby_wrapper":
+  user { 'ubuntu':
+    ensure => present
+  }
+  exec { 'passwd':
+    command => 'passwd -l ubuntu'
+  }
+  exec { 'adduser':
+    command => 'adduser ubuntu admin'
+  }
+  exec { 'chown':
+    command => 'chown ubuntu:root /var/www'
+  }
+
+  file { "/var/www/redch/shared/env_ruby_wrapper":
     ensure => present,
     owner  => root,
     group  => root,
     mode   => 755,
     source => "puppet:///modules/redch/env_ruby_wrapper",
-  }
-  package {
-    "bundler":
-      provider => gem
   }
 }
