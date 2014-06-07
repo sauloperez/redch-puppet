83d73690ae3ed7d587097f023d38b02d1590e01c
Pau Pérez <saulopefa@gmail.com>
2014-06-07 13:33:59 +0200

    Install redch to the default /var/www/redch

--- modules/redch/manifests/init.pp
+++ modules/redch/manifests/init.pp
@@ -1,23 +1,10 @@
 class redch {
-  file {
-    ["/var/redch/",
-     "/var/redch/shared/",
-     "/var/redch/shared/config/",
-     "/var/redch/shared/log/",
-     "/var/redch/shared/tmp/",
-     "/var/redch/shared/pids/"]:
-    ensure => directory,
-    owner => ubuntu,
-    group => ubuntu,
-    mode => 775
-  }
   file { "/var/redch/shared/env_ruby_wrapper":
     ensure => present,
     owner  => root,
     group  => root,
     mode   => 755,
     source => "puppet:///modules/redch/env_ruby_wrapper",
-    require => File["/var/redch/shared/"]
   }
   package {
     "bundler":
